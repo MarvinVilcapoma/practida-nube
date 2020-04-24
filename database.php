4968fe1b8f3d2049d26dde8ebc2119502e85bfe0
Friman Vilcapoma Flores <friman.vilcapoma@tecsup.edu.pe>
2020-04-23 20:24:08 -0500

    Primer Commit Heroku

--- database.php
+++ database.php
@@ -2,13 +2,11 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  '',
-  'practica_nubes'
+  'otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  'zmhna4o85svoj3bx',
+  'zssqv3icd0avrq03',
+  'zk745kjclkckwqdm'
 ) or die(mysqli_erro($mysqli));
-
-
 
 
 ?>
